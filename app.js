c4089305c698e3d6f3aaa45656f47b7131ecb0be
manuel69man <68189182+manuel69man@users.noreply.github.com>
2025-03-17 23:10:36 -0300

    Add files via upload
    Creando Array para almacenar nombres

---
+++ app.js
@@ -0,0 +1,6 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaramos un array sin elementos.
+let amigos=[];
+let arrayElementosLista=[];
+let listadeAmigos = document.getElementById("listaAmigos");
+
