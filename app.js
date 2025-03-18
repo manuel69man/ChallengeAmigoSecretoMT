09a5efbf76ca05898bd2de02925712aff1ab2bbb
manuel69man <68189182+manuel69man@users.noreply.github.com>
2025-03-17 23:31:59 -0300

    Add files via upload
    Se agrego la funcion para sortear amigo secreto, mostrarlo en pantalla. Por ultimo se permite el reseteo del Juego para reiniciarlo

--- app.js
+++ app.js
@@ -33,3 +33,36 @@ function limpiarAmigo(){
     let amigo = document.getElementById("amigo");
     amigo.value="";
 }
+
+function limpiarAmigo(){
+    let amigo = document.getElementById("amigo");
+    amigo.value="";
+}
+
+function sortearAmigo(){
+    //Usamos getElementById para accesar al objeto de document resultado
+    let resulatadoObjeto = document.getElementById("resultado");
+    // Validamos que existan amigos en el array
+    if (amigos.length>0){
+        // Generamos el amigo aleatoriamente usando Math.tandom y Math.floor
+        let posAmigoSorteado = parseInt(Math.floor(Math.random()*amigos.length));
+        let amigoSorteado= amigos[posAmigoSorteado];
+        //Mostramos el amigo secreto usando innerHTML
+        resulatadoObjeto.innerHTML="Tu Amigo secreto es: "+amigoSorteado;
+
+        resetJuego();
+
+
+
+    }else {
+        /*Si el array de amigos esta vacio, quiere decir que o no hemos jugado o queremos sortear otro amigo
+        por lo que nos aseguramos que este mostrando un amigo secreto viejo, de esta manera podemos jugar las veces que queramos*/
+        resulatadoObjeto.innerHTML="";
+    } 
+}
+
+function resetJuego(){
+    amigos=[];
+    listadeAmigos.innerHTML="";
+
+}
